7f791e63ecd59e15bcbbd944683e28844b6445bb
irregularunit <pawsfiction@gmail.com>
2023-03-16 19:56:16 +0100

    Add configuration file

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+__version__: Final[str] = '0.0.1'
+__author__: Final[str] = 'irregularunit'
+__license__: Final[str] = 'Creative Commons Attribution-Noncommercial-Share Alike license'
