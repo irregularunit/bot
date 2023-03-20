8630c522d92c6a85840eef8978de9e461167bb4a
irregularunit <127952894+irregularunit@users.noreply.github.com>
2023-03-20 12:56:40 +0100

    Adjust project license

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from typing import Final
 
 __version__: Final[str] = '0.0.1'
 __author__: Final[str] = 'irregularunit'
-__license__: Final[str] = 'Creative Commons Attribution-Noncommercial-Share Alike license'
+__license__: Final[str] = 'Attribution-NonCommercial-ShareAlike 4.0 International'
