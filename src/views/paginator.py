1156069f31cd4aac68d0fe6e4c1fa7e8184cc224
irregularunit <pawsfiction@gmail.com>
2023-04-01 07:50:08 +0200

    Add resuable paginator

---
+++ src/views/paginator.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from copy import deepcopy
+from typing import TYPE_CHECKING, Optional
+
+from discord import ButtonStyle, Embed, File, Interaction
+from discord.ui import Button, View, button
+
+if TYPE_CHECKING:
+    from bot import Bot
+
+__all__: tuple[str, ...] = ("Item", "Paginator")
+
+
+class Item:
+    __slots__ = ("content", "embed", "_files")
+
+    def __init__(
+        self,
+        *,
+        content: Optional[str] = None,
+        embed: Optional[Embed] = None,
+        files: Optional[list[File]] = None,
+    ):
+        self.content = content
+        self.embed = embed
+        self._files = files or []
+
+    @property
+    def files(self):
+        return [deepcopy(f) for f in self._files]
+
+
+class Paginator(View):
+    """Simple embed and file paginator view"""
+
+    def __init__(self, bot: Bot, *items: Item):
+        super().__init__()
+        self.bot = bot
+        self.items = items
+        self.page = 0
+        self.emojis = {
+            "first": "⏮",
+            "back": "◀",
+            "next": "▶",
+            "skip": "⏭",
+        }
+
+        for child in self.children:
+            if isinstance(child, Button):
+                if child.style == ButtonStyle.secondary:
+                    child.style = ButtonStyle.primary
+                child.emoji = self.emojis[child.callback.callback.__name__]
+
+    async def edit(self, iact: Interaction, *, page: int):
+        self.page = page
+        unit = self.items[page]
+        await iact.response.edit_message(
+            content=unit.content, embed=unit.embed, attachments=unit.files
+        )
+
+    @button()
+    async def first(self, iact: Interaction, button: Button[Paginator]):
+        await self.edit(iact, page=0)
+
+    @button()
+    async def back(self, iact: Interaction, button: Button[Paginator]):
+        await self.edit(iact, page=max(self.page - 1, 0))
+
+    @button()
+    async def next(self, iact: Interaction, button: Button[Paginator]):
+        await self.edit(iact, page=min(self.page + 1, len(self.items) - 1))
+
+    @button()
+    async def skip(self, iact: Interaction, button: Button[Paginator]):
+        await self.edit(iact, page=len(self.items) - 1)
